48d40a684a0337ea4fd36bfa15249edfe0b6db93
kidwalom <cnweke@ucmerce.edu>
2014-12-08 09:10:21 -0800

    intial test of program

--- project2/sgemm-optimize.c
+++ project2/sgemm-optimize.c
@@ -1,6 +1,10 @@
 void sgemm( int m, int n, float *A, float *C )
 {
-
-// FILL-IN
-
+  for( int k = 0; k < m; k++ ){
+    for( int i = 0; i < n; i++ ){
+      for( int j = 0; j < m; j++ ){
+        C[i+j*m] += A[i+k*m] * A[j+k*m];
+      }
+    }
+  }
 }
