e6210fbb58a9f504bc48d0a8ec65c92b7de7f732
Your Name <youremail@domain.com>
2014-12-10 04:35:50 -0800

    made cache requirement bigger; function doesnt tank (prefetch)

--- project2/benchmark.c
+++ project2/benchmark.c
@@ -26,11 +26,11 @@ int main( int argc, char **argv )
 {
   srand(time(NULL));
 
-  	for( int n = 32; n < 100; n = n+1+n/3 )
+  	for( int n = 32; n < 8192; n = n+1+n/3 )
 //	for( int n = 40; n <  41; n = n+1+n/3 )
 	{
   /* Try different m */
-  for( int m = 32; m <  100; m = m+1+m/3 )
+  for( int m = 32; m <  8192; m = m+1+m/3 )
 //for( int m = 48; m <  49; m = m+1+m/3 )
   {
     /* Allocate and fill 2 random matrices A, C */
