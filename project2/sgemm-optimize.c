cba77594c076905b13771df8ae573cfff11bb012
kidwalom <cnweke@ucmerce.edu>
2014-12-09 22:55:40 -0800

    got it to be like 5X faster

--- project2/sgemm-optimize.c
+++ project2/sgemm-optimize.c
@@ -1,10 +1,9 @@
 void sgemm( int m, int n, float *A, float *C )
 {
-  for( int k = 0; k < m; k++ ){
-    for( int i = 0; i < n; i++ ){
-      for( int j = 0; j < m; j++ ){
+  __builtin_prefetch(&A);
+  __builtin_prefetch(&C);
+  for( int j = 0; j < m; j++ )
+    for( int k = 0; k < n; k++ )
+      for( int i = 0; i < m; i++ )
         C[i+j*m] += A[i+k*m] * A[j+k*m];
-      }
-    }
-  }
 }
