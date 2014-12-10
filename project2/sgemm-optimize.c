37a6535dff51d8947752d22a320a73f690430b19
Your Name <youremail@domain.com>
2014-12-10 04:23:39 -0800

    got it slightly faster using define and prefectchig

--- project2/sgemm-optimize.c
+++ project2/sgemm-optimize.c
@@ -1,9 +1,15 @@
+
+#define a A[i+k*m] 
+#define b A[j+k*m]
+#define c C[i+j*m]
+
 void sgemm( int m, int n, float *A, float *C )
 {
   __builtin_prefetch(&A);
   __builtin_prefetch(&C);
+
   for( int j = 0; j < m; j++ )
     for( int k = 0; k < n; k++ )
       for( int i = 0; i < m; i++ )
-        C[i+j*m] += A[i+k*m] * A[j+k*m];
+        c += a*b;
 }
