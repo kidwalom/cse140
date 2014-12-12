6a2e18deb0a1279769ebf2c3efcffc2faef04a38
kidwalom <cnweke@ucmerce.edu>
2014-12-12 02:21:41 -0800

    added all notes and im now at 3-5x speed

--- project2/sgemm-optimize.c
+++ project2/sgemm-optimize.c
@@ -10,11 +10,14 @@ defines a colum major transpose macro */
 #define COLMAJOR(i,j,m,n) (j*m)+i
 /* include useful library */
 #include <string.h>
+/*this is the SSE intrincix library that gives 4 instuctions at time 4X speed [possibly]*/
 #include "xmmintrin.h"
 /* transposes and pad the matrix to an offest of four so as to be able to do calulation much easier */
 float* transpad(int m, int n, float *inputfloat)
 {
-	/* if true return 4(namtrix size not offeset of 4) else 0*/
+	/* if true return then next factor of 4 [e.g. if its 4*5 it becomes 4*8
+	//(namtrix size not offeset of 4) else 0*/
+	
 	int mpad = (m & ~0x03) + ((m & 0x03) ? 4:0);
 	int npad = (n & ~0x03) + ((n & 0x03) ? 4:0);
 
@@ -30,10 +33,13 @@ float* transpad(int m, int n, float *inputfloat)
 }
 void sgemm( int m, int n, float *A, float *C )
 {
+	/* creates the padded matrixes for a and c */
 	float *padded_A = transpad(m, n, A);
-/*	//try this if all else fails
+	float *padded_C = transpad(m,n,C);
+/*	//try this if all else fails 
+	//dont ry this might make slower on different computers
 	 __builtin_prefetch(&A);
-	 __builtin_prefetch(&C); */
+	 __builtin_prefetch(&C);*/
 
 	  for( int j = 0; j < m; j++ )
 	  {
@@ -41,9 +47,28 @@ void sgemm( int m, int n, float *A, float *C )
 	    {
 	      for( int i = 0; i < m; i++ )
 	      {
+	      	/*going to change this multiplier to be the padded c = padded a (colum major)  *padded a (row major) */
 	        	c+=a*b;
 	    	}
 		}
 	}
 	free (padded_A);
 }
+
+/*
+	// this 
+	  for( int j = 0; j < mpad; j++ )
+	  {
+	    for( int k = 0; k < npad; k++ )
+	    {
+	      for( int i = 0; i < mpad; i++ )
+	      {
+	        	c+=a*b;
+	    	}
+		}
+	}
+	free (padded_A);
+}
+
+
+*/
